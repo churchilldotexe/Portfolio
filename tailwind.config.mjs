7fa0edc9dc87dc5346b4ff97acbb24d38d999171
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-08-22 05:54:07 +0800

    chore: scaffolding project page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "elevate-light": "0 8px 6px 0 rgba(0,0,0,.37), -6px -4px 10px white",
+        "elevate-dark": "0 8px 6px 0 rgba(0,0,0,0.2), -6px -4px 10px black",
+      },
+    },
   },
   plugins: [],
 };

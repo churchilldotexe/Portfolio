7a5417be5c972c3654cf5bdf713f63da2d5afc24
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-08-23 14:19:09 +0800

    style: added mobile design

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = {
+  facebook: "https://facebook.com/churchilljay",
+  github: "https://github.com/Churchillexe",
+  twitter: "https://x.com/ChurchillJay3",
+  linkedin: "https://www.linkedin.com/in/churchill-jay-sungcados-90b49a29a/",
+} as const;

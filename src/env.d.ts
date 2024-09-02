4f67ab9b3f13407001a6e21a8c90260bc5dc6f38
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-09-02 23:18:41 +0800

    fix: astro clouflare run time

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,19 @@
 /// <reference path="../.astro/types.d.ts" />
 ///// <reference types="astro/client" />
 /// <reference types="@clerk/astro/env" />
+import type { AdvancedRuntime } from "@astrojs/cloudflare";
+
+interface Runtime {
+  env: {
+    CLIENT_ID: string;
+    CLIENT_SECRET: string;
+  };
+}
+
+declare global {
+  declare namespace App {
+    interface Locals {
+      runtime: Runtime;
+    }
+  }
+}

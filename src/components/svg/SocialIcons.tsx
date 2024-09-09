87167f13c4393020c86884e4e354dbfa8245c71e
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-09-09 18:52:33 +0800

    style: setting up glow on hover

--- src/components/svg/SocialIcons.tsx
+++ src/components/svg/SocialIcons.tsx
@@ -37,3 +37,12 @@ export function LinkedIn({ ...props }: IconProp) {
     </svg>
   );
 }
+
+export function VercelIcon({ ...props }: IconProp) {
+  return (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <title>Vercel</title>
+      <path d="M24 22.525H0l12-21.05 12 21.05z"></path>
+    </svg>
+  );
+}

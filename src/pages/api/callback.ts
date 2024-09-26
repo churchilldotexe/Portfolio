9387818d5ed470083883faca0736cfa0a4b8079c
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-09-27 00:29:40 +0800

    refactor: callback api to return searchParams redirect instead of Response

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -1,6 +1,7 @@
 export const prerender = false;
 
 import { COOKIES_OPTIONS, COOKIES_PROPERTIES } from "@/lib/constants";
+import { fetcher } from "@/lib/utils";
 import { ratelimit } from "@/server/ratelimitter";
 import { validateUserUseCase } from "@/server/use-case/auth";
 import { verifyAccessToken } from "@/server/use-case/auth/token-use-cases";
@@ -20,56 +21,71 @@ const userAuthSchema = z.object({
   avatar_url: z.string().optional(),
 });
 
-const responseReturn = (message: string, status: ResponseInit["status"]) => {
-  return new Response(
-    JSON.stringify({
-      message: message,
-    }),
-    { status }
-  );
+export type LoginParamsTypes =
+  | { status: "error"; message: string }
+  | { status: "success" }
+  | { status: "failed" };
+
+const createPathWithParams = (url: string | URL, paramsValue: LoginParamsTypes): string => {
+  const params = new URLSearchParams();
+  params.set("login", paramsValue.status);
+
+  if (paramsValue.status === "error") {
+    params.set("message", paramsValue.message);
+  }
+  return `${url}?${params.toString()}`;
 };
 
-export const GET: APIRoute = async ({ redirect, cookies, request }): Promise<Response> => {
-  try {
-    const githubUrl = new URL(request.url);
-    const code = githubUrl.searchParams.get("code");
-    const stateParamsValue = githubUrl.searchParams.get("state");
+export const GET: APIRoute = async ({ cookies, redirect, request }): Promise<Response> => {
+  const githubUrl = new URL(request.url);
+  const code = githubUrl.searchParams.get("code");
+  const stateParamsValue = githubUrl.searchParams.get("state");
 
-    // to prevent redirect loop and request spam
-    const identifier = request.headers.get("X-Forwarded-For") ?? import.meta.env.CLIENT_ID;
+  // to prevent redirect loop and request spam
+  const identifier = request.headers.get("X-Forwarded-For") ?? import.meta.env.CLIENT_ID;
 
-    const { success } = await ratelimit.limit(identifier);
+  const { success } = await ratelimit.limit(identifier);
 
-    if (!success) {
-      return responseReturn("Rate Limit reach Please wait for another 10mins", 429);
-    }
+  if (!success) {
+    const redirectUrl = createPathWithParams("/dashboard", {
+      status: "error",
+      message: "Rate Limit reach Please wait for another 10mins",
+    });
+    return redirect(redirectUrl, 307);
+  }
 
-    const cookieAccessToken = cookies.get(COOKIES_PROPERTIES.ACCESSTOKEN)?.value;
-    if (cookieAccessToken) {
-      // decode the token
-      return redirect("/dashboard", 302);
-    }
+  const cookieAccessToken = cookies.get(COOKIES_PROPERTIES.ACCESSTOKEN)?.value;
+  if (cookieAccessToken) {
+    // accessToken still accessible
+    const redirectUrl = createPathWithParams("/dashboard", { status: "success" });
+    return redirect(redirectUrl, 307);
+  }
 
-    const cookieValue = cookies.get(COOKIES_PROPERTIES.STATE)?.value;
-    const verifiedCookie = await verifyAccessToken<{ state: string }>(cookieValue);
+  const cookieValue = cookies.get(COOKIES_PROPERTIES.STATE)?.value;
+  const verifiedCookie = await verifyAccessToken<{ state: string }>(cookieValue);
 
-    // for errors and redirect
-    const logAndRedirect = (errorLogMessage: string): Response => {
-      console.error(errorLogMessage);
-      return redirect("/api/redirect", 302);
-    };
+  // for errors and redirect
+  const redirectToLogin = (errorMessage: string): Response => {
+    console.error(errorMessage);
+    const redirectUrl = createPathWithParams("/api/redirect", {
+      status: "error",
+      message: errorMessage,
+    });
+    return redirect(redirectUrl, 307);
+  };
 
-    //redirect cookie if invalid(expired or mismatch)
-    if (!verifiedCookie) {
-      return logAndRedirect("invalid verifiedCookie");
-    }
+  //redirect cookie if invalid(expired or mismatch)
+  if (!verifiedCookie) {
+    return redirectToLogin("invalid verifiedCookie");
+  }
 
-    //to handle possible cross site attack redirect back to github auth
-    if (stateParamsValue !== verifiedCookie.state) {
-      return logAndRedirect("cookie mismatched verifiedCookie");
-    }
-
-    const response = await fetch("https://github.com/login/oauth/access_token", {
+  //to handle possible cross site attack redirect back to github auth
+  if (stateParamsValue !== verifiedCookie.state) {
+    return redirectToLogin("cookie mismatched verifiedCookie");
+  }
+  const response = await fetcher(
+    "https://github.com/login/oauth/access_token",
+    {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -80,56 +96,76 @@ export const GET: APIRoute = async ({ redirect, cookies, request }): Promise<Res
         client_secret: import.meta.env.CLIENT_SECRET,
         code,
       }),
-    });
+    },
+    accessTokenSchema
+  );
+  if (!response.success) {
+    if (response.error instanceof ZodError) {
+      console.error(response.error);
+      const redirectUrl = createPathWithParams("/dashboard", {
+        status: "error",
+        message: "Parsing Error: unable to get the correct data",
+      });
+      return redirect(redirectUrl, 307);
+    } else {
+      console.error(response.error);
 
-    if (!response.ok) {
-      return responseReturn(`${response.status}: ${response.statusText}`, 500);
-    }
-
-    const data = (await response.json()) as unknown;
-    const parsedData = accessTokenSchema.parse(data);
-
-    //to retry and get the correct scope and access_token
-    if (parsedData.scope.toLowerCase() !== "user:email" || !parsedData.access_token) {
-      return logAndRedirect("invalid access token and scope");
-    }
-
-    const userResponse = await fetch("https://api.github.com/user", {
-      headers: {
-        Authorization: `${parsedData.token_type} ${parsedData.access_token}`,
-      },
-    });
-
-    if (!userResponse.ok) {
-      return responseReturn(`${userResponse.status}: ${userResponse.statusText}`, 500);
-    }
-
-    const userData = (await userResponse.json()) as unknown;
-    const parsedUserData = userAuthSchema.parse(userData);
-    const { email, id, login, avatar_url } = parsedUserData;
-    const accessToken = await validateUserUseCase({
-      email,
-      authId: id,
-      avatarUrl: avatar_url,
-      displayName: login,
-    });
-
-    if (!accessToken) {
-      throw new Error("unable to validate the user");
-    }
-
-    cookies.set(COOKIES_PROPERTIES.ACCESSTOKEN, accessToken, COOKIES_OPTIONS);
-    cookies.delete(COOKIES_PROPERTIES.STATE, COOKIES_OPTIONS);
-
-    return redirect("/dashboard?login=success", 302);
-  } catch (e) {
-    if (e instanceof Error) {
-      responseReturn(`${e.name}: ${e.cause}. ${e.message}. ${e.stack}`, 500);
-    }
-    if (e instanceof ZodError) {
-      responseReturn(`Validation Error: ${e.errors.map((err) => err.message).join(",")}`, 400);
+      const redirectUrl = createPathWithParams("/dashboard", {
+        status: "error",
+        message: `${response.error.name} Error occured`,
+      });
+      return redirect(redirectUrl, 307);
     }
   }
 
-  return redirect("/", 302);
+  //to retry and get the correct scope and access_token
+  if (response.data.scope.toLowerCase() !== "user:email" || !response.data.access_token) {
+    return redirectToLogin("invalid access token and scope");
+  }
+
+  const userResponse = await fetcher(
+    "https://api.github.com/user",
+    {
+      headers: {
+        Authorization: `${response.data.token_type} ${response.data.access_token}`,
+      },
+    },
+    userAuthSchema
+  );
+
+  if (!userResponse.success) {
+    if (userResponse.error instanceof ZodError) {
+      console.error(userResponse.error);
+      const redirectUrl = createPathWithParams("/dashboard", {
+        status: "error",
+        message: "Parsing Error: unable to get the correct data",
+      });
+      return redirect(redirectUrl, 307);
+    } else {
+      console.error(userResponse.error);
+      const redirectUrl = createPathWithParams("/dashboard", {
+        status: "error",
+        message: `${userResponse.error.name} Error occured`,
+      });
+      return redirect(redirectUrl, 307);
+    }
+  }
+
+  const { email, id, login, avatar_url } = userResponse.data;
+  const accessToken = await validateUserUseCase({
+    email,
+    authId: id,
+    avatarUrl: avatar_url,
+    displayName: login,
+  });
+
+  if (!accessToken) {
+    throw new Error("unable to validate the user");
+  }
+
+  cookies.set(COOKIES_PROPERTIES.ACCESSTOKEN, accessToken, COOKIES_OPTIONS);
+  cookies.delete(COOKIES_PROPERTIES.STATE, COOKIES_OPTIONS);
+
+  const redirectUrl = createPathWithParams("/dashboard", { status: "success" });
+  return redirect(redirectUrl, 307);
 };

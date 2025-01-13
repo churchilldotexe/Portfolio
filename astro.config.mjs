bf736b19691b5143f46a8526cdf30e074ab58a62
churchilldotexe <churchillexe@gmail.com>
2025-01-13 23:04:48 +0800

    chore: change to server outpute

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
   prefetch: true,
   adapter: vercel(),
   integrations: [tailwind(), react()],
+  output: "server",
   vite: {
     ssr: {
       external: ["node:buffer"],

f56d9e9279cc629214180fae1a523fe7d1189300
churchilldotexe <churchillexe@gmail.com>
2025-01-13 22:28:24 +0800

    fix: change to astrojs/vercel instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,4 +19,3 @@ export default defineConfig({
   },
   security: { checkOrigin: true },
 });
-
